bfc955c7ef04964b43f4d1f6f1ee2ae58f4a3997
Ryan Bemowski <rrbemo@gmail.com>
2022-07-09 19:41:08 -0500

    Adding telemetry plots and changed to a dashboard style

--- server.R
+++ server.R
@@ -4,47 +4,37 @@ library(plotly)
 library(jsonlite)
 
 #all_laps <- read_csv("2022_australia.csv") 
-#host_name <- "http://127.0.0.1:5000"
-host_name <- "http://127.0.0.1"
+host_name <- "http://127.0.0.1:5000"
+#host_name <- "http://127.0.0.1"
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
   # Build the UI
   output$ui_year_options <- renderUI({
-    years <- fromJSON(paste0(host_name, "/fast_f1_years"))
-    ui <- column(4, tags$h2("Year"),
-                 radioGroupButtons("year_option",
-                                   "Available Years",
-                                   label = "Season",
-                                   choices = years,
-                                   direction = "vertical"))
+    years <- fromJSON(paste0(host_name, "/fast_f1_years")) %>%
+      sort(decreasing = TRUE)
+    ui <- pickerInput("year_option",
+                      label = "Season Year",
+                      choices = years,
+                      options = list(
+                        'live-search' = TRUE
+                      ))
     return(ui)
   })
-  
-  events <- reactive({
-    req(input$year_option)
-    events <- fromJSON(paste0(host_name, "/fast_f1_events/", input$year_option))
-  })
-  
-  session_data <- reactive({
-    req(input$session_option)
-    session_data <- fromJSON(URLencode(paste0(host_name, "/fast_f1_laps/", input$year_option, "/", input$track_option, "/", input$session_option)))
-  })
-  
   output$ui_track_options <- renderUI({
     #data_url <- paste0(host_name, "/fast_f1_events/", input$year_options)
     #print(data_url)
     events <- events()
-    ui <- column(4, tags$h2("Track"),
-           radioGroupButtons("track_option", 
-                             "Tracks for year",
-                             label = "Grand Prix",
-                             choices = events$EventName,
-                             direction = "vertical"))
+    ui <- pickerInput("track_option", 
+                      label = "Grand Prix",
+                      choices = events$EventName,
+                      selected = NA,
+                      options = list(
+                       'live-search' = TRUE
+                      ))
     return(ui)
   })
-  
   output$ui_session_options <- renderUI({
     #data_url <- paste0(host_name, "/fast_f1_events/", input$year_options)
     #print(data_url)
@@ -55,20 +45,49 @@ server <- function(input, output) {
     #  gather("Session", "SessionName") %>%
     #  select(SessionName) %>%
     #  filter(SessionName != "None")
-    ui <- column(4, tags$h2("Session"),
-                 radioGroupButtons("session_option", 
-                                   "Sessions in event",
-                                   label = "Session",
-                                   #choices = sessions$SessionName,
-                                   choices = c("Race"),
-                                   selected = "Race",
-                                   direction = "vertical"))
+    ui <- pickerInput("session_option", 
+                      label = "Session",
+                      #choices = sessions$SessionName,
+                      choices = c("Race"),
+                      selected = "Race")
     return(ui)
   })
+  # output$ui_driver_to_offset <- renderUI({
+  #   req(input$session_option)
+  #   ui <- pickerInput("driver_to_offset", "Driver to offset",
+  #                     car_data$CarNumber)
+  # })
+  # output$ui_driver_offset <- renderUI({
+  #   req(input$driver_to_offset)
+  #   ui <- sliderInput("driver_offset", "Telemetry Offset",
+  #                     min = -100,
+  #                     max = 100,
+  #                     value = 0)
+  # })
   
-  output$plotlyd1 <- renderPlotly({
+  events <- reactive({
+    req(input$year_option)
+    events <- fromJSON(paste0(host_name, "/fast_f1_events/", input$year_option))
+  })
+  session_data <- reactive({
     req(input$session_option)
-    laps <- session_data()%>%
+    the_url <- URLencode(paste0(host_name, "/fast_f1_laps/", input$year_option, "/", input$track_option, "/", input$session_option))
+    session_data <- fromJSON(the_url)
+  })
+  car_data <- reactive({
+    req(input$session_option)
+    the_url <- URLencode(paste0(host_name, "/fast_f1_car_data/", input$year_option, "/", input$track_option, "/", input$session_option))
+    session_data <- fromJSON(the_url)
+  })
+  pos_data <- reactive({
+    req(input$session_option)
+    the_url <- URLencode(paste0(host_name, "/fast_f1_pos_data/", input$year_option, "/", input$track_option, "/", input$session_option))
+    session_data <- fromJSON(the_url)
+  })
+  
+  output$plotly_time_to_leader <- renderPlotly({
+    req(input$session_option)
+    laps <- session_data() %>%
       mutate(PitInLap = ifelse(is.na(PitInTime),
                                FALSE,
                                TRUE)) %>%
@@ -121,7 +140,47 @@ server <- function(input, output) {
                           zeroline = FALSE),
              xaxis = list(zeroline = FALSE)) # Reverse the y axis so top car is ahead.
   })
-  
+  output$plotly_car_data <- renderPlotly({
+    req(input$session_option)
+    df <- car_data() %>%
+      select(c(CarNumber, SessionTime, Throttle, Brake, Speed)) %>%
+      mutate(Brake = as.numeric(Brake) * 100) 
+    #%>%
+     # filter(CarNumber == '20')
+
+    plot_ly(data = df, type = "scatter") %>%
+      add_trace(name = ~CarNumber, 
+                x = ~SessionTime, y = ~Speed,
+                mode = "lines",
+                legendgroup = ~CarNumber,
+                connectgaps = TRUE) %>%
+      #add_trace(name = "Throttle", 
+      #          x = ~SessionTime, y = ~Throttle,
+      #          mode = "lines",
+      #          legendgroup = ~CarNumber,
+      #          connectgaps = TRUE) %>%
+      #add_trace(name = "Brake", 
+      #          x = ~SessionTime, y = ~Brake,
+      #          mode = "lines",
+      #          legendgroup = ~CarNumber,
+      #          connectgaps = TRUE) %>%
+      layout(yaxis = list(zeroline = FALSE),
+             xaxis = list(zeroline = FALSE)) # Reverse the y axis so top car is ahead.
+  })
+  output$plotly_pos_data <- renderPlotly({
+    req(input$session_option)
+    df <- pos_data() %>%
+      select(c(CarNumber, SessionTime, X, Y, Z)) %>%
+      filter(CarNumber == '20')
+    
+    plot_ly(data = df, type = "scatter") %>%
+      add_trace(name = "Speed", 
+                x = ~X, y = ~Y,
+                mode = "markers",
+                connectgaps = TRUE) %>%
+      layout(yaxis = list(zeroline = FALSE),
+             xaxis = list(zeroline = FALSE)) # Reverse the y axis so top car is ahead.
+  })
   #observeEvent(events(), {
   #  updateGroupButtons(inputId = "track_options", choices = events()$EventName)
   #})
--- ui.R
+++ ui.R
@@ -1,21 +1,57 @@
 library(shiny)
 library(shinycssloaders)
-#library(shinydashboard)
+library(shinydashboard)
 library(shinyWidgets)
 #library(shinyjs)
 #library(shinythemes)
 library(plotly)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  fluidRow(
-    # Application title
-    titlePanel("F1 Lap Analysis"),
-    fluidRow(fluidRow(plotlyOutput("plotlyd1") %>% withSpinner())),
-    fluidRow(
+ui <- dashboardPage(
+  dashboardHeader(title = "F1 Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
       uiOutput("ui_year_options"),
       uiOutput("ui_track_options"),
-      uiOutput("ui_session_options")
+      uiOutput("ui_session_options"),
+      menuItem("Time To Leader", tabName = "timeToLeader", icon = icon("chart-line")),
+      menuItem("Car Data", tabName = "car_data", icon = icon("magnifying-glass-chart")),
+      menuItem("Car Position", tabName = "pos_data", icon = icon("magnifying-glass-chart"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "timeToLeader",
+              h2("Time to leader"),
+              fluidRow(
+                # Application title
+                box(width = 12,
+                    plotlyOutput("plotly_time_to_leader") %>% withSpinner()
+                )
+              )
+      ),
+      tabItem(tabName = "car_data",
+              h2("Car Data Analysis"),
+              fluidRow(
+                box(width = 12,
+                    plotlyOutput("plotly_car_data") %>% withSpinner()
+                )
+              )#,
+              #fluidRow(
+              #  box(width = 12,
+              #      uiOutput("ui_driver_to_offset"),
+              #      uiOutput("ui_driver_offset")
+              #  )
+              #)
+      ),
+      tabItem(tabName = "pos_data",
+              h2("Car Position Analysis"),
+              fluidRow(
+                box(width = 12,
+                    plotlyOutput("plotly_pos_data") %>% withSpinner()
+                )
+              )
+      )
     )
   )
 )
