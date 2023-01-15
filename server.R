19612d458b2839972f9a5a5f6f2cf7deefd2f181
Ryan Bemowski <rrbemo@gmail.com>
2023-01-15 16:22:20 -0600

    Major updates to the dynamic UI for comparing car/laps. Each compared text has its own dynamic row. Rows still need to be fixed so that they do not create multiple rows on small screens.

--- server.R
+++ server.R
@@ -11,7 +11,7 @@ host_name <- "http://127.0.0.1:5000"
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  # Build the UI
+  ## Dynamic UI elements
   output$ui_year_options <- renderUI({
     years <- fromJSON(paste0(host_name, "/fast_f1_years")) %>%
       sort(decreasing = TRUE)
@@ -60,11 +60,12 @@ server <- function(input, output) {
                       selected = "16",
                       choices = unique(session_data()$DriverNumber))
   })
-  output$ui_driver_one_offset <- renderUI({
-    ui <- sliderInput("driver_one_offset", "Telemetry Offset",
-                      min = -100,
-                      max = 100,
-                      value = 0)
+  output$ui_lap_one_options <- renderUI({
+    req(input$driver_one_option)
+    ui <- pickerInput("lap_one_option",
+                      label = "Lap",
+                      selected = "5",
+                      choices = order(unique(session_data()$LapNumber)))
   })
   output$ui_driver_two_options <-renderUI({
     req(input$driver_one_option)
@@ -73,19 +74,78 @@ server <- function(input, output) {
                       selected = "55",
                       choices = unique(session_data()$DriverNumber))
   })
-  output$ui_driver_two_offset <- renderUI({
-    ui <- sliderInput("driver_two_offset", "Telemetry Offset",
-                      min = -100,
-                      max = 100,
-                      value = 0)
-  })
-  output$ui_lap_options <- renderUI({
-    req(input$driver_one_option)
-    ui <- pickerInput("lap_option",
+  output$ui_lap_two_options <- renderUI({
+    req(input$driver_two_option)
+    #req(input$synclap)
+    ui <- pickerInput("lap_two_option",
                       label = "Lap",
                       selected = "5",
-                      choices = session_data()$LapNumber)
+                      choices = order(unique(session_data()$LapNumber)))
   })
+  output$ui_car_lap_compare <- renderUI({
+    req(car_lap_data_one)
+    req(car_lap_data_two)
+    req(car_lap_details_data_one)
+    req(car_lap_details_data_two)
+    
+    row_size <- c("vs." = "h1",
+                  "Compound(laps)" = "p",
+                  "Lap" = "h2",
+                  "Lap Time" = "h3",
+                  "Sector 1" = "p",
+                  "Sector 2" = "p",
+                  "Sector 3" = "p",
+                  "Top Speed" = "h3",
+                  "Avg. Speed" = "h3",
+                  "Low Speed" = "p")
+    car1 <- car_lap_ui_data(car_lap_details_data_one(), car_lap_data_one())
+    car2 <- car_lap_ui_data(car_lap_details_data_two(), car_lap_data_two())
+    ui <- NULL
+    for (name in names(row_size)) {
+      ui_row <- fluidRow(width = 12,
+                         column(width = 5,
+                                style = "text-align:left;",
+                                do.call(row_size[name], list(car1[name]))),
+                         column(width = 2,
+                                style = "text-align:center;",
+                                do.call(row_size[name], list(name))),
+                         column(width = 5,
+                                style = "text-align:right;",
+                                do.call(row_size[name], list(car2[name])))
+                         )
+      if (is.null(ui)) {
+        ui <- ui_row
+      } else {
+        ui <- paste(ui, ui_row)
+      }
+    }
+    tagList(HTML(ui))
+  })
+  
+  ## Helper functions for Car/lap details data UI
+  car_lap_ui_data <- function(car_lap_details, car_lap_tel) {
+    c("vs." = paste(car_lap_details$Team, "-",
+                    car_lap_details$DriverNumber,
+                    car_lap_details$Driver),
+      "Compound(laps)" = paste0(car_lap_details$Compound, "(", 
+                                car_lap_details$TyreLife, ")"),
+      "Lap" = car_lap_details$LapNumber,
+      "Lap Time" = format_time(car_lap_details$LapTime),
+      "Sector 1" = format_time(car_lap_details$Sector1Time),
+      "Sector 2" = format_time(car_lap_details$Sector2Time),
+      "Sector 3" = format_time(car_lap_details$Sector3Time),
+      "Top Speed" = paste(max(car_lap_tel$Speed), "kph"),
+      "Avg. Speed" = paste(round(mean(car_lap_tel$Speed), 1), "kph"),
+      "Low Speed" = paste(min(car_lap_tel$Speed), "kph")
+    )
+  }
+  format_time <- function(ftime) {
+    sprintf("%02d:%02.3f",
+            minute(as.period(as.duration(milliseconds(ftime)))),
+            second(as.period(as.duration(milliseconds(ftime)))))
+  }
+  
+  ## Reactive functions
   events <- reactive({
     req(input$year_option)
     events <- fromJSON(paste0(host_name, "/fast_f1_events/", input$year_option))
@@ -94,6 +154,7 @@ server <- function(input, output) {
     req(input$session_option)
     the_url <- URLencode(paste0(host_name, "/fast_f1_laps/", input$year_option, "/", input$track_option, "/", input$session_option))
     session_data <- fromJSON(the_url)
+    #print(session_data)
   })
   car_data <- reactive({
     req(input$session_option)
@@ -121,6 +182,61 @@ server <- function(input, output) {
     # #%>%
      # filter(CarNumber %in% c(input$driver_one, input$driver_two))
   })
+  car_lap_data_one <- reactive({
+    req(session_data)
+    req(input$driver_one_option)
+    req(input$lap_one_option)
+    
+    df <- car_lap_data(input$driver_one_option, 
+                       input$lap_one_option)
+  })
+  car_lap_details_data_one <- reactive({
+    req(session_data)
+    req(input$driver_one_option)
+    req(input$lap_one_option)
+    
+    df <- car_lap_details_data(input$driver_one_option, 
+                               input$lap_one_option)
+  })
+  car_lap_data_two <- reactive({
+    req(session_data)
+    req(input$driver_two_option)
+    req(input$lap_two_option)
+    
+    df <- car_lap_data(input$driver_two_option, 
+                       input$lap_two_option)
+  })
+  car_lap_details_data_two <- reactive({
+    req(session_data)
+    req(input$driver_two_option)
+    req(input$lap_two_option)
+    
+    df <- car_lap_details_data(input$driver_two_option, 
+                               input$lap_two_option)
+  })
+  ## Helper functions for reactive driver_lap functions
+  car_lap_details_data <- function(car_num, lap_num) {
+    lap_df <- session_data() %>%
+      filter((DriverNumber == car_num &
+                LapNumber == lap_num))
+  }
+  car_lap_data <- function(car_num, lap_num) {
+    lap_df <- car_lap_details_data(car_num, lap_num)
+    
+    tel_df <- car_data() %>% #telemetry() %>%
+      filter(CarNumber %in% car_num) %>% 
+      select(c(CarNumber, SessionTime, Throttle, Brake, Speed)) %>%
+      mutate(Brake = as.numeric(Brake) * 100)
+    
+    df <- tel_df %>%
+      filter(SessionTime >= lap_df$LapStartTime[lap_df$DriverNumber == car_num]
+             & SessionTime < lap_df$Sector3SessionTime[lap_df$DriverNumber == car_num]
+             & CarNumber == car_num) %>%
+      mutate(TELEMETRY_NUMBER = row_number(),
+             TELEMETRY_COUNT = n(),
+             TELEMETRY_PROP = TELEMETRY_NUMBER/TELEMETRY_COUNT,
+             group_name = paste0("Driver:", car_num, " - Lap:", lap_num))
+  }
   
   output$plotly_time_to_leader <- renderPlotly({
     req(input$session_option)
@@ -178,49 +294,63 @@ server <- function(input, output) {
              xaxis = list(zeroline = FALSE)) # Reverse the y axis so top car is ahead.
   })
   output$plotly_car_data <- renderPlotly({
-    req(input$session_option)
-    req(input$driver_one_option)
-    req(input$lap_option)
+    # req(input$session_option)
+    # req(input$driver_one_option)
+    # req(input$lap_one_option)
     
-    car_number <- c(input$driver_one_option, input$driver_two_option) #, input$driver_two_option)
-    lap_number <- input$lap_option
-    print(input$session_option)
-    print(car_number)
-    print(lap_number)
-    lap_df <- session_data() %>%
-      filter(DriverNumber %in% car_number,
-             LapNumber == lap_number)
+    # car_number <- c(input$driver_one_option, input$driver_two_option) #, input$driver_two_option)
+    # lap_number <- c(input$lap_one_option, input$lap_two_option)
+    # 
+    # print(input$session_option)
+    # print(car_number)
+    # print(lap_number)
+    # lap_df_one <- session_data() %>%
+    #   filter((DriverNumber == car_number[1] &
+    #            LapNumber == lap_number[1]))
+    # lap_df_two <- session_data() %>%
+    #   filter((DriverNumber == car_number[2] &
+    #            LapNumber == lap_number[2]))
+    # 
+    # tel_df <- car_data() %>% #telemetry() %>%
+    #   filter(CarNumber %in% car_number) %>% 
+    #   select(c(CarNumber, SessionTime, Throttle, Brake, Speed)) %>%
+    #   mutate(Brake = as.numeric(Brake) * 100)
     
-    tel_df <- car_data() %>% #telemetry() %>%
-      filter(CarNumber %in% car_number) %>% 
-      select(c(CarNumber, SessionTime, Throttle, Brake, Speed)) %>%
-      mutate(Brake = as.numeric(Brake) * 100)
+    #print(tel_df)
     
-    print(tel_df)
+    ## TODO: Need to find a way to store lap number so I can show the same care and different laps.
+    # df_one <- tel_df %>%
+    #   filter((SessionTime >= lap_df_one$LapStartTime[lap_df_one$DriverNumber == car_number[1]] 
+    #             & SessionTime < lap_df_one$Sector3SessionTime[lap_df_one$DriverNumber == car_number[1]]
+    #             & CarNumber == car_number[1])) %>%
+    #   mutate(TELEMETRY_NUMBER = row_number(),
+    #          TELEMETRY_COUNT = n(),
+    #          TELEMETRY_PROP = TELEMETRY_NUMBER/TELEMETRY_COUNT,
+    #          group_name = paste0("Driver:", car_number[1], " - Lap:", lap_number[1]))
     
-    df <- tel_df %>%
-      filter((SessionTime >= lap_df$LapStartTime[lap_df$DriverNumber == car_number[1]] 
-                & SessionTime < lap_df$Sector3SessionTime[lap_df$DriverNumber == car_number[1]]
-                & CarNumber == car_number[1])
-             | (SessionTime >= lap_df$LapStartTime[lap_df$DriverNumber == car_number[2]] 
-                & SessionTime < lap_df$Sector3SessionTime[lap_df$DriverNumber == car_number[2]]
-                & CarNumber == car_number[2])) %>%
-      ## Compare by start lap time
-     group_by(CarNumber) %>%
-     mutate(TELEMETRY_NUMBER = row_number(),
-            TELEMETRY_COUNT = n(),
-            TELEMETRY_PROP = TELEMETRY_NUMBER/TELEMETRY_COUNT) %>%
-     ungroup()
+    # df_two <- tel_df %>%
+    #   filter((SessionTime >= lap_df_two$LapStartTime[lap_df_two$DriverNumber == car_number[2]] 
+    #             & SessionTime < lap_df_two$Sector3SessionTime[lap_df_two$DriverNumber == car_number[2]]
+    #             & CarNumber == car_number[2])) %>%
+    #   mutate(TELEMETRY_NUMBER = row_number(),
+    #          TELEMETRY_COUNT = n(),
+    #          TELEMETRY_PROP = TELEMETRY_NUMBER/TELEMETRY_COUNT,
+    #          group_name = paste0("Driver:", car_number[2], " - Lap:", lap_number[2]))
+    req(car_lap_data_one)
+    req(car_lap_data_two)
+    
+    df <- car_lap_data_one() %>%
+      rbind(car_lap_data_two())
     
     #print(df)
     #%>%
      # filter(CarNumber == '20')
 
     plot_ly(data = df, type = "scatter") %>%
-      add_trace(name = ~CarNumber, 
+      add_trace(name = ~group_name, 
                 x = ~TELEMETRY_PROP, y = ~Speed,
                 mode = "lines",
-                legendgroup = ~CarNumber,
+                legendgroup = ~group_name,
                 connectgaps = TRUE) %>%
       #add_trace(name = "Throttle", 
       #          x = ~SessionTime, y = ~Throttle,
--- ui.R
+++ ui.R
@@ -24,6 +24,10 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName = "timeToLeader",
               h2("Ryan Bemowski's F1 Time to leader"),
+              p("The time to leader plot shows relative position of each driver to the leader.
+              The further down the driver's line falls, the further behind the leader they 
+              are becoming. When lines cross, a position was exchanged. Pit stops, or in/out
+              laps are also recorded here."),
               fluidRow(
                 # Application title
                 box(width = 12,
@@ -33,21 +37,59 @@ ui <- dashboardPage(
       ),
       tabItem(tabName = "car_data",
               h2("Ryan Bemowski's F1 car speed comparison per lap"),
+              p("The Car Speed Comparison plot shows the difference between two car's speed
+                throughout a lap, or different laps. This can be especially helpful when 
+                comparing the same driver to themselves on different laps or comparing 
+                similarly quick drivers to eachother."),
+              p("To keep the data as comparable as possible, the speed is plotted as a 
+                proportion of the lap completed. This will help line up apexes, but will 
+                distort how quickly one lap may have been completed compared to the other."),
+              p("Note: Each time a new session is loaded, the telemetry data for all drivers 
+                and laps must be loaded. This process can take a bit longer than expected. After 
+                the initial session is loaded new driver and lap selections should load quickly."),
               fluidRow(
                 box(width = 12,
-                    box(width = 5, 
-                        uiOutput("ui_driver_one_options")),  #Car1 selection 
-                    box(width = 5,
-                        uiOutput("ui_driver_two_options")),  #Car2 selection,
-                    box(width = 2,
-                        uiOutput("ui_lap_options"))          #Lap number selection
-                    )
+                    column(width = 4,
+                           uiOutput("ui_driver_one_options")),
+                    column(width = 1,
+                           uiOutput("ui_lap_one_options")),
+                    column(width = 1,
+                           checkboxInput("synclap", "Sync Lap?", value = TRUE)),
+                    column(width = 4,
+                           uiOutput("ui_driver_two_options")),
+                    column(width = 2,
+                        uiOutput("ui_lap_two_options"))          #Lap number selection
+                )
               ),
               fluidRow(
                 box(width = 12,
                     plotlyOutput("plotly_car_data") %>% withSpinner()
                 )
-              )#,
+              ),
+              fluidRow(
+                box(width = 12,
+                    uiOutput("ui_car_lap_compare")
+                    # column(width = 5,
+                    #        style = "text-align:left;",
+                    #        uiOutput("ui_driver_one_info")),
+                    # column(width = 2,
+                    #        style = "text-align:center;",
+                    #        h1("Driver"),
+                    #        p("Compound(laps)"),
+                    #        h2("Lap"),
+                    #        h3("Lap Time"),
+                    #        p("Sector 1"),
+                    #        p("Sector 2"),
+                    #        p("Sector 3"),
+                    #        h3("Top Speed"),
+                    #        h3("Average Speed"),
+                    #        p("Min Speed")),
+                    # column(width = 5,
+                    #        style = "text-align:right;",
+                    #        uiOutput("ui_driver_two_info"))
+                    )
+              )
+              #,
               #fluidRow(
               #  box(
               #    uiOutput("ui_driver_one"),
@@ -57,9 +99,9 @@ ui <- dashboardPage(
               #    uiOutput("ui_driver_two"),
               #    uiOutput("ui_driver_two_offset") 
               #  )#,
-                #box(width = 12,
-                #    DTOutput('telemetry_table') 
-                #)
+              #box(width = 12,
+              #    DTOutput('telemetry_table') 
+              #)
               #)
       ),
       tabItem(tabName = "pos_data",
